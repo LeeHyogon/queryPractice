3a1166ca28ffc8f67d8fc85331117621ec2119df
이효곤 <gon109m@gmail.com>
2023-04-16 15:05:57 +0900

    kk

---
+++ src/main/java/com/study/queryPractice/QueryPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.study.queryPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/queryPractice/QueryPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.queryPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
